3c1e64efb7e100647eeb2e23f394ad0b0556e02a
JayDub21 <justin.wofford.21@gmail.com>
2020-01-15 09:16:35 -0600

    added variables to index.js

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const questions = [
+let profileImg;
+let gitHubUserName;
+let userCity;
+let userGitHubProfile;
+let userBlog;
+let userBio;
+let userRepositories;
+let userFollowers;
+let userGHStars;
+let userFollowing;
 
-];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "GitHub Username?",
+            name: "username"
+        },
+        {
+            type: "list",
+            message: "What is your favorite color?",
+            choices: [
+                "green",
+                "blue",
+                "pink",
+                "red"
+            ],
+            name: "color"
+        },
+    ])
+    .then(function (response) {
 
-function writeToFile(fileName, data) {
+        const gitHubUser = response.username.split(" ").join(" ") + '.json'
 
-}
+        fs.writeFile(
+            gitHubUser, JSON.stringify(response, null, '\t'), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("File Saved!");
+                }
 
-function init() {
+            });
+    });
 
-    init();
+        // const questions = [
+
+        // ];
+
+        // function writeToFile(fileName, data) {
+
+        // }
+
+        // function init() {
+
+        //     init();
