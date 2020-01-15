2da1b11a00c4180ca7efb7bed98bf8b929d09a72
JayDub21 <justin.wofford.21@gmail.com>
2020-01-15 11:27:33 -0600

    added GH Stars function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require('axios').default;
 
 let profileImg;
 let gitHubUserName;
@@ -12,6 +13,10 @@ let userFollowers;
 let userGHStars;
 let userFollowing;
 
+
+//==============================================
+// Inquirer asks for Username and fave color
+//==============================================
 inquirer
     .prompt([
         {
@@ -31,30 +36,64 @@ inquirer
             name: "color"
         },
     ])
-    .then(function (response) {
+    .then(function (userInput) {
 
-        const gitHubUser = response.username.split(" ").join(" ") + '.json'
+        const gitHubUser = userInput.username.split(" ").join(" ") + '.json'
 
         fs.writeFile(
-            gitHubUser, JSON.stringify(response, null, '\t'), function (err) {
+            gitHubUser, JSON.stringify(userInput, null, '\t'), function (err) {
                 if (err) {
                     console.log(err);
                 }
-                else {
-                    console.log("File Saved!");
-                }
+
+                console.log("File Saved!");
+
+                //==============================================
+                // Sets up URL for main GH API Call
+                //==============================================
+                const ghURL = "https://api.github.com/users/" + userInput.username;
+                console.log(ghURL);
+                //==============================================
+                // Sets up URL for starred GH API Call
+                //==============================================
+                const ghStarURL = "https://api.github.com/users/" + userInput.username + "/starred";
+                console.log(ghStarURL);
+
+                //==============================================
+                // Put URL in function
+                //==============================================
+                ghAPI(ghURL);
+                ghStarAPI(ghStarURL);
 
             });
     });
 
-        // const questions = [
 
-        // ];
+function ghAPI(ghURL) {
 
-        // function writeToFile(fileName, data) {
+    axios.get(ghURL)
+        .then(function (response) {
+            // console.log(response.data);
 
-        // }
 
-        // function init() {
+            let profileImg = (response.data.avatar_url + ".png");
+            let gitHubUserName = (response.data.login);
+            let userCity = (response.data.location);
+            let userGitHubProfile = (response.data.html_url);
+            let userBlog = (response.data.blog);
+            let userBio = (response.data.bio);
+            let userRepositories = (response.data.public_repos);
+            let userFollowers = (response.data.followers);
+            let userFollowing = (response.data.following);
+            // let userGHStars =  Create another axios call for - ;
 
-        //     init();
+        });
+};
+
+function ghStarAPI(ghStarURL) {
+
+    axios.get(ghStarURL)
+        .then(function (responseStars) {
+            console.log(responseStars.data.length);
+        });
+};
