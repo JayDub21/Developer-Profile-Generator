2ce9999deaca584b840e5a21284d2094e9837ddd
JayDub21 <Justin.Wofford.21@gmail.com>
2021-02-08 08:30:43 -0600

    updated ghQueryStars function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 const axios = require('axios').default;
 const getHTML = require('./generateHTML');
 const pdf = require('html-pdf');
@@ -11,88 +11,78 @@ const pdf = require('html-pdf');
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "GitHub Username?",
-            name: "username"
-        },
-        {
-            type: "list",
-            message: "What is your favorite color?",
-            choices: [
-                "green",
-                "blue",
-                "pink",
-                "red"
-            ],
-            name: "color"
-        },
-    ])
-    .then(function (userInput) {
+  .prompt([
+    {
+      type: 'input',
+      message: 'GitHub Username?',
+      name: 'username',
+    },
+    {
+      type: 'list',
+      message: 'What is your favorite color?',
+      choices: ['green', 'blue', 'pink', 'red'],
+      name: 'color',
+    },
+  ])
+  .then(function (userInput) {
+    //==============================================
+    // Set up URLs for API calls below
+    //==============================================
+    const queryUrl = `https://api.github.com/users/${userInput.username}`;
+    const queryStarUrl = `https://api.github.com/users/${userInput.username}/starred`;
 
-        //==============================================
-        // Set up URLs for API calls below
-        //==============================================
-        const queryUrl = `https://api.github.com/users/${userInput.username}`;
-        const queryStarUrl = `https://api.github.com/users/${userInput.username}/starred`;
-
-
-        ghquery(queryUrl).then(function (data) {
-            ghQueryStars(queryStarUrl).then(function (responseStars) {
-
-                //========================================================
-                // Taking generateHTML.js file and converting it to .PDF
-                //========================================================
-                var options = { format: 'Letter' };
-                pdf.create(getHTML(userInput, responseStars, data), options).toFile(`./${userInput.username}.pdf`, function (err, res) {
-                    if (err) return console.log(err);
-                    console.log(res);
-                });
-            })
-        })
-
-
+    ghquery(queryUrl).then(function (data) {
+      ghQueryStars(queryStarUrl).then(function (responseStars) {
+        //========================================================
+        // Taking generateHTML.js file and converting it to .PDF
+        //========================================================
+        var options = { format: 'Letter' };
+        pdf
+          .create(getHTML(userInput, responseStars, data), options)
+          .toFile(`./${userInput.username}.pdf`, function (err, res) {
+            if (err) return console.log(err);
+            console.log(res);
+          });
+      });
     });
+  });
 
 //==============================================
 // Main API call
 //==============================================
 function ghquery(queryUrl) {
-    // console.log(queryUrl);
-    return axios.get(queryUrl)
-        .then(function (response) {
-            // console.log(respone.data);
+  // console.log(queryUrl);
+  return axios.get(queryUrl).then(function (response) {
+    console.log(response.data);
 
-            let data = {
-                fullName: (response.data.name),
-                profileImg: (response.data.avatar_url + ".png"),
-                gitHubUsername: (response.data.login),
-                userCity: (response.data.location),
-                userGitHubProfile: (response.data.html_url),
-                userBlog: (response.data.blog),
-                userBio: (response.data.bio),
-                userRepos: (response.data.public_repos),
-                userFollowers: (response.data.followers),
-                userFollowing: (response.data.following)
-            };
+    let data = {
+      fullName: response.data.name,
+      profileImg: response.data.avatar_url + '.png',
+      gitHubUsername: response.data.login,
+      userCity: response.data.location,
+      userGitHubProfile: response.data.html_url,
+      userBlog: response.data.blog,
+      userBio: response.data.bio,
+      userRepos: response.data.public_repos,
+      userFollowers: response.data.followers,
+      userFollowing: response.data.following,
+    };
 
-
-
-
-            return data;
-        });
-};
+    return data;
+  });
+}
 
 //==============================================
 // API Call for # of starred
 //==============================================
 function ghQueryStars(queryStarUrl) {
+  return axios.get(queryStarUrl).then(function (response) {
+    console.log('Starred Projects: ', response.data.length);
 
-    return axios.get(queryStarUrl)
-        .then(function (responseStars) {
-            // console.log(responseStars.data.length);
+    let responseStars = {
+      stars: response.data.length,
+    };
 
-            return responseStars.data.length;
-        });
-};
+    return responseStars;
+  });
+}
